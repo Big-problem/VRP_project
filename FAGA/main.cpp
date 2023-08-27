263c84b30eaaf08faa68d83b2963bd03cf6810ee
Big-problem <117372204+Big-problem@users.noreply.github.com>
2023-08-27 14:27:46 +0800

    Change variable name

--- FAGA/main.cpp
+++ FAGA/main.cpp
@@ -28,30 +28,32 @@ int main()
     //system("FAGA.exe");
 
     // Start
-    double res = -1.0;
-    vector<Route> ret;
+    double shortest_distance = -1.0;
+    vector<Route> best_solution;
     for(int i=1;i<=10;i++)
     {
         cout<<i<<": ";
         FAGA ans(50,500,200,0.2);
         ans.run_algo();
         ans.test();
-        if(res < 0 || res > ans.get_total_distance_traveled()){
-            ret = ans.get_solution();
-            res = ans.get_total_distance_traveled();
+        if(shortest_distance < 0 || shortest_distance > ans.get_total_distance_traveled()){
+            best_solution = ans.get_solution();
+            shortest_distance = ans.get_total_distance_traveled();
         }
         cout<<"\n";
     }
     analyze();
 
+    // Get the result file
     ofstream outstream;
     outstream.open("R101_25_result.txt");
     if(!outstream.fail()){
-        for(int i = 0; i < ret.size(); ++i) {
-            ret[i].print_file(outstream);
+        for(int i = 0; i < best_solution.size(); ++i) {
+            best_solution[i].print_file(outstream);
         }
     }
     else cout << "Failed to open file!\n";
+
     /*FAGA ans(50,500,200,0.2);
     ans.test();*/
     return 0;
